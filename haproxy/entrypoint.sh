38adfb32acf70082440eaf1e671d4a0aecec6abe
mgautam <gautam.raw@gmail.com>
2020-05-15 21:38:03 +1000

    prometheus user permissions; haproxy scrape uri

--- haproxy/entrypoint.sh
+++ haproxy/entrypoint.sh
@@ -6,6 +6,6 @@ cd /root
 
 #wget https://github.com/prometheus/haproxy_exporter/releases/download/v0.10.0/haproxy_exporter-0.10.0.linux-amd64.tar.gz
 tar xzvf haproxy_exporter-0.10.0.linux-amd64.tar.gz
-./haproxy_exporter-0.10.0.linux-amd64/haproxy_exporter
+./haproxy_exporter-0.10.0.linux-amd64/haproxy_exporter --haproxy.scrape-uri="http://localhost:8404/stats;csv"
 
 #/bin/bash
