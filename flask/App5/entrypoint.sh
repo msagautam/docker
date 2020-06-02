3977f65cefca9fc453be0cad63da05cc112d3300
GautamManoharan <gautam.raw@gmail.com>
2020-06-02 19:59:26 +1000

    uwsgi configuration fixed

--- flask/App5/entrypoint.sh
+++ flask/App5/entrypoint.sh
@@ -6,14 +6,17 @@ chmod 777 upload_data_files
 
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/myfilesApp.conf /etc/nginx/sites-enabled/
-ln -s /etc/uwsgi/apps-available/uwsgi.ini /etc/uwsgi/apps-enabled/
 service nginx restart
-service uwsgi restart
 
-nohup uwsgi /etc/uwsgi/apps-available/uwsgi.ini &
+service uwsgi stop
+ln -s /etc/uwsgi/apps-available/uwsgi.ini /etc/uwsgi/apps-enabled/
+#service uwsgi restart
+nohup uwsgi /etc/uwsgi/apps-enabled/uwsgi.ini &
 
-usermod -a -G www-data
+usermod -a -G www-data www-data
 chown -R www-data:www-data /tmp/
+
+#uwsgi /etc/uwsgi/apps-available/uwsgi.ini
 
 #python3 -m myfilesApp 
 
@@ -24,5 +27,6 @@ chown -R www-data:www-data /tmp/
 #/bin/bash
 
 # For never ending program
+tail -f /var/log/nginx/myfilesApp.log
 #tail -f /var/log/uwsgi/*
-uwsgi --http-socket :8080 --plugin python -w myfilesApp:app
+#uwsgi --http-socket :8080 --plugin python -w myfilesApp:app
